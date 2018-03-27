41b322b5dc23f7dab806ab128718e94f24db03c0
JUSTIN-X220\justi <justinmklam@gmail.com>
2018-03-26 23:24:43 -0700

    Web form works as expected. Now need to make it look pretty

--- app.py
+++ app.py
@@ -1,30 +1,33 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, flash
 import recipeConverter as rc
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '7d441f27d441f28567d441f2b6176a'
 
-@app.route('/')
-def main_form():
-    return render_template('form.html')
-    # '<form action="submit" id="textform" method="post"><textarea name="text">Hello World!</textarea><input type="submit" value="Submit"></form>'
+@app.route('/', methods=['GET', 'POST'])
+def hello():
+    input_text = "Enter your recipe in volumetric units (cups, tablespoons, teaspoons)"
 
-@app.route('/submit', methods=['POST'])
-def submit_textarea():
-    # store the given text in a variable
-    text = request.form.get("text")
+    if request.method == 'POST':
+        # store the given text in a variable
+        text = request.form.get("text")
 
-    # split the text to get each line in a list
-    text2 = text.split('\n')
+        # split the text to get each line in a list
+        text2 = text.split('\n')
 
-    recipe = rc.RecipeConverter()
-    text_converted = recipe.parse_recipe(text2)
+        recipe = rc.RecipeConverter()
+        text_converted = recipe.parse_recipe(text2)
 
-    # change the text (add 'Hi' to each new line)
-    text_changed = '<br>'.join(text_converted)
-    # (i used <br> to materialize a newline in the returned value)
+        # change the text (add 'Hi' to each new line)
+        # input_text = '\n'.join(text_converted)
+        input_text = text
+        # (i used <br> to materialize a newline in the returned value)
 
+        for line in text_converted:
+            flash(line)
 
-    return "{}".format(text_changed)
+    return render_template('form.html', textarea=input_text)
 
 if __name__ == '__main__':
     app.run()
