e7cd3f4e6d0ef546dc0687c889e1f4c43b36a20b
Justin Lam <justinmklam@gmail.com>
2020-03-22 13:07:55 -0700

    Extract components from line

--- recipeconverter/utils/parser.py
+++ recipeconverter/utils/parser.py
@@ -1,9 +1,19 @@
 from fractions import Fraction
-
+import re
 
 def convert_ingredient_volume_to_mass(line: str) -> str:
     return "foo"
 
+def parse_line(line:str) -> tuple:
+    p = re.compile(r"(.+?)(?=[a-zA-z])([a-zA-Z]\w+)")
+    m = p.findall(line)
+    print(m)
+
+    amount = m[0][0].strip()
+    unit = m[0][1].strip()
+    ingredient = m[1][1].strip()
+
+    return amount, unit, ingredient
 
 def fraction_to_float(fraction: str) -> float:
     """Convert string representation of a fraction to float
--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -7,6 +7,12 @@ def test_convert_ingredient_line():
     assert parser.convert_ingredient_volume_to_mass("1 cup flour") == "120 g flour"
 
 
+def test_parse_ingredient_line():
+    amount, unit, ingredient = parser.parse_line("1 cup flour")
+    assert amount == "1"
+    assert unit == "cup"
+    assert ingredient == "flour"
+
 def test_fraction_to_float():
     assert parser.fraction_to_float("1/2") == 0.5
     assert parser.fraction_to_float("1 1/4") == 1.25
