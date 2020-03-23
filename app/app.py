36a70a88a6ae1d7f8a97b4a68666d41fd429e265
Justin Lam <justinmklam@gmail.com>
2020-03-23 10:00:30 -0700

    Move template text from flask to html

--- app/app.py
+++ app/app.py
@@ -1,25 +1,15 @@
-from flask import Flask, request, render_template, flash
+from flask import Flask, request, render_template
 import recipeconverter as rc
-# import win32clipboard
+
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '7d441f27d441f28567d441fb6176a'
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route('/', methods=["GET"])
 def hello():
-    input_text = """1⁄2 cup butter
-1 cup sugar
-2 eggs, beaten
-3 bananas, finely crushed (for serious and extreme moist and delicious, try 4 bananas)
-1 1⁄2 cups flour
-1 teaspoon baking soda
-1⁄2 teaspoon salt
-1⁄2 teaspoon vanilla (optional)"""
-
-    multiplier = 1.0
-
-    return render_template('form.html', textarea=input_text, multiplier=multiplier)
+    return render_template('form.html')
 
 
 @app.route("/convert", methods=["POST"])
