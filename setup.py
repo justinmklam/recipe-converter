a0129c18a9be0d92d2151163fdac4a6eef5bf257
Justin Lam <justinmklam@gmail.com>
2020-03-27 18:42:00 -0700

    Update version number in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="recipeconverter",
-    version="0.1.0",
+    version="2.0.0",
     description="A tool to convert baking ingredients from volume to weight",
     author="Justin Lam",
     author_email="contact@justinmklam.com",
